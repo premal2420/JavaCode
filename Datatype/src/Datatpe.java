0e11d36448f36b97de305d4637173ca2e97385a4
premal2420 <57621724+premal2420@users.noreply.github.com>
2022-04-11 02:34:34 +0530

    Add files via upload

---
+++ Datatype/src/Datatpe.java
@@ -0,0 +1,21 @@
+public class Datatpe {
+
+        public static void main (String [] args) {
+
+            //Datatype int,char,Float,String,double,long,Boolean
+
+            int Roll_no = 1;
+
+            char Grade = 'P';
+
+            Float Marks = 98.90f;
+
+            String Name = "Premal";
+
+            Long Number = 7745878765l;
+
+            Boolean isValid = false;
+
+            System.out.println(Grade);
+        }
+}
