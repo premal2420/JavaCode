3595f2f0c4c53663a8dd0be8e90b509b91dca5a6
premal2420 <57621724+premal2420@users.noreply.github.com>
2022-04-13 13:42:53 +0530

    Add files via upload

---
+++ method/src/Method.java
@@ -0,0 +1,12 @@
+public class Method {
+    public static void main(String [] args){
+
+        System.out.println(avg(6,10));
+
+    }
+
+   static int avg (int First,int Second){
+        int sum = First + Second ;
+        return sum/2;
+    }
+}
